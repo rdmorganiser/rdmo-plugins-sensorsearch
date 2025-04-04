3634747b5cecee838e72cf1d3b77119b4317416a
David Wallace <david.wallace@tu-darmstadt.de>
2025-04-04 15:15:17 +0200

    style: rename vars
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo_sensorsearch/config.py
+++ rdmo_sensorsearch/config.py
@@ -45,30 +45,30 @@ def load_config():
     """
     # load settings
     try:
-        CONFIG_FILE_NAME = settings.SENSORS_SEARCH_PROVIDER_CONFIG_FILE_NAME
+        config_file_name = settings.SENSORS_SEARCH_PROVIDER_CONFIG_FILE_NAME
     except AttributeError:
-        CONFIG_FILE_NAME = "config.toml"
+        config_file_name = "config.toml"
 
     try:
-        CONFIG_FILE_PATH = settings.SENSORS_SEARCH_PROVIDER_CONFIG_FILE_PATH
+        config_file_path = settings.SENSORS_SEARCH_PROVIDER_CONFIG_FILE_PATH
     except AttributeError:
-        CONFIG_FILE_PATH = None
+        config_file_path = None
 
     # override by environment variables or use defaults
-    CONFIG_FILE_NAME = os.getenv("SENSORS_SEARCH_PROVIDER_CONFIG_FILE_NAME", CONFIG_FILE_NAME)
-    CONFIG_FILE_PATH = os.getenv("SENSORS_SEARCH_PROVIDER_CONFIG_FILE_PATH", CONFIG_FILE_PATH)
+    config_file_name = os.getenv("SENSORS_SEARCH_PROVIDER_CONFIG_FILE_NAME", config_file_name)
+    config_file_path = os.getenv("SENSORS_SEARCH_PROVIDER_CONFIG_FILE_PATH", config_file_path)
 
-    if CONFIG_FILE_PATH is None:
-        CONFIG_FILE_PATH = os.path.join(Path(__file__).parent, CONFIG_FILE_NAME)
+    if config_file_path is None:
+        config_file_path = os.path.join(Path(__file__).parent, config_file_name)
 
-    logger.debug("Try to open configuration file: %s", CONFIG_FILE_PATH)
+    logger.debug("Try to open configuration file: %s", config_file_path)
 
     try:
-        with open(CONFIG_FILE_PATH, "rb") as config_file:
+        with open(config_file_path, "rb") as config_file:
             return tomllib.load(config_file)
     except (FileNotFoundError, PermissionError) as e:
-        logger.error("Connot open configuration file: %s", CONFIG_FILE_PATH)
+        logger.error("Cannot open configuration file: %s", config_file_path)
         raise e from e
     except tomllib.TOMLDecodeError as e:
-        logger.error("Failed to decode configuration file: %s", CONFIG_FILE_PATH)
+        logger.error("Failed to decode configuration file: %s", config_file_path)
         raise e from e
