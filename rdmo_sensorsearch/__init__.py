0e5a79ad334f80f0411a4a563d700d286e958992
David Wallace <david.wallace@tu-darmstadt.de>
2026-06-17 11:01:31 +0200

    build: change pacakge versioning to hatch-vcs
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo_sensorsearch/__init__.py
+++ rdmo_sensorsearch/__init__.py
@@ -1,7 +1,12 @@
 # SPDX-FileCopyrightText: 2023 - 2024 Hannes Fuchs (GFZ) <hannes.fuchs@gfz-potsdam.de>
 # SPDX-FileCopyrightText: 2023 - 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
-#
+# SPDX-FileCopyrightText: 2025 - 2026 RDMO Community and individual contributors
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "1.1.1"
-VERSION = __version__
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _version
+
+try:
+    VERSION = __version__ = _version(__package__)
+except PackageNotFoundError:
+    VERSION = __version__ = "0.0.0+unknown"
